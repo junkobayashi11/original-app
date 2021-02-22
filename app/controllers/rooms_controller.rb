61de12f1a67493562dd4959bd34b042d043e9a86
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-02-22 22:35:01 +0900

    room_controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,42 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all.order(created_at: "DESC")
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to action: :index
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to action: :index
+    else
+      reder :edit
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:room_name, :prefecture_id, :host_date, :municipalities).merge(user_id: current_user.id)
+  end
+end
